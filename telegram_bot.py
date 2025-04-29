2f1966c9f90f37dd71d9f71ae6ca18b8cef7193c
lZenaku <98990813+lZenaku@users.noreply.github.com>
2025-04-28 21:22:42 -0500

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,5 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, Filters, ContextTypes
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 import asyncio
 import platform
 
